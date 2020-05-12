86fdfab172ea4e025efa881fec016bcdfa58817e
oliviathevampire <sindrefagerheim53@gmail.com>
2020-05-12 22:39:38 +0200

    idk

--- src/main/java/de/ellpeck/rockbottom/launcher/Launcher.java
+++ src/main/java/de/ellpeck/rockbottom/launcher/Launcher.java
@@ -1,11 +1,33 @@
 package de.ellpeck.rockbottom.launcher;
 
+import de.ellpeck.rockbottom.Main;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Launcher {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.NORTH);
+
+        JButton launchGameButton = new JButton("Play");
+        launchGameButton.setBounds((1280-300)/2, 700-100, 300, 70);
+        launchGameButton.setFont(launchGameButton.getFont().deriveFont(30.0F));
+        launchGameButton.addActionListener(e -> {
+            frame.setVisible(false);
+            Main.main(args);
+        });
+        frame.getContentPane().add(launchGameButton, BorderLayout.CENTER);
+
+        frame.setSize(1280,720);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
 }
